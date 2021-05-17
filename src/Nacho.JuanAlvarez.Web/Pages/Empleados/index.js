800b077c4b160fa7cf35be984d2e683b7bbfe010
pablo <pdpalma@hotmail.com>
2021-05-17 17:31:08 -0300

    filters

--- src/Nacho.JuanAlvarez.Web/Pages/Empleados/index.js
+++ src/Nacho.JuanAlvarez.Web/Pages/Empleados/index.js
@@ -153,4 +153,38 @@
         e.preventDefault();
         createModal.open();
     });
+
+    function loadFilters(filter, columnIndex) {
+
+        // Declare variables
+        var table, tr, td, i, txtValue;
+
+        table = document.getElementById("EmpleadosTable");
+        tr = table.getElementsByTagName("tr");
+
+        // Loop through all table rows, and hide those who don't match the search query
+        for (i = 0; i < tr.length; i++) {
+            td = tr[i].getElementsByTagName("td")[columnIndex];
+            if (td) {
+                txtValue = td.textContent || td.innerText;
+                if (txtValue.toUpperCase().indexOf(filter.toUpperCase()) > -1) {
+                    tr[i].style.display = "";
+                } else {
+                    tr[i].style.display = "none";
+                }
+            }
+        }
+    }
+
+    $("#paisFilter").on('keyup change', function () {
+        loadFilters(this.value, 4);
+    });
+
+    $("#areaFilter").on('keyup change', function () {
+        loadFilters(this.value, 7);
+    });
+
+    $("#nroBusquedaFilter").on('keyup change', function () {
+        loadFilters(this.value, 6);
+    });
 });
