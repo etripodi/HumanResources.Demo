660f6c0d889f232dc858fdd17654a95026d27a16
Esteban Tripodi <etripodi@gmail.com>
2021-09-01 18:29:14 -0300

    gfdgdf

--- src/Nacho.JuanAlvarez.Web/Pages/Empleados/Index.js
+++ src/Nacho.JuanAlvarez.Web/Pages/Empleados/Index.js
@@ -2,13 +2,36 @@
     var l = abp.localization.getResource('JuanAlvarez');
     var createModal = new abp.ModalManager(abp.appPath + 'Empleados/CreateModal');
     var editModal = new abp.ModalManager(abp.appPath + 'Empleados/EditModal');
+    
+    var DOWNLOAD_ENDPOINT = "/download";
+    var downloadForm = $("form#DownloadFile");
+    
+    downloadForm.submit(function (event) {
+        event.preventDefault();
+        
+        var fileName = $("#fileName").val().trim();
+        
+        var downloadWindow = window.open(
+            
+          DOWNLOAD_ENDPOINT + "/" + fileName,
+          "_blank"  
+        );
+        
+        downloadWindow.focus();
+    })
+    
+    $("#UploadFileDto_File").change(function () {
+       var fileName = $(this)[0].files[0].name;
+       
+       $("#UploadFileDto_Name").val(fileName);
+    });
 
     var dataTable = $('#EmpleadosTable').DataTable(
         abp.libs.datatables.normalizeConfiguration({
             serverSide: true,
             paging: true,
             order: [[1, "asc"]],
-            searching: true,
+            searching: false,
             scrollX: true,
             ajax: abp.libs.datatables.createAjax(nacho.juanAlvarez.empleados.empleado.getList),
             columnDefs: [
@@ -118,8 +141,6 @@
         })
     );
 
-
-
     createModal.onResult(function () {
         dataTable.ajax.reload();
     });
@@ -128,11 +149,42 @@
         dataTable.ajax.reload();
     });
 
-
     $('#NewEmpleadoButton').click(function (e) {
         e.preventDefault();
         createModal.open();
     });
 
+    function loadFilters(filter, columnIndex) {
 
+        // Declare variables
+        var table, tr, td, i, txtValue;
+
+        table = document.getElementById("EmpleadosTable");
+        tr = table.getElementsByTagName("tr");
+
+        // Loop through all table rows, and hide those who don't match the search query
+        for (i = 0; i < tr.length; i++) {
+            td = tr[i].getElementsByTagName("td")[columnIndex];
+            if (td) {
+                txtValue = td.textContent || td.innerText;
+                if (txtValue.toUpperCase().indexOf(filter.toUpperCase()) > -1) {
+                    tr[i].style.display = "";
+                } else {
+                    tr[i].style.display = "none";
+                }
+            }
+        }
+    }
+
+    $("#paisFilter").on('keyup change', function () {
+        loadFilters(this.value, 4 /*Column index*/);
+    });
+
+    $("#areaFilter").on('keyup change', function () {
+        loadFilters(this.value, 7 /*Column index*/);
+    });
+
+    $("#nroBusquedaFilter").on('keyup change', function () {
+        loadFilters(this.value, 6 /*Column index*/);
+    });
 });
